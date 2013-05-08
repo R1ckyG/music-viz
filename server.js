620040587fcfc63fbccf3308acf891fd0c4c90b2
EC2 Default User <ec2-user@ip-10-170-94-129.us-west-1.compute.internal>
2013-05-08 00:37:14 +0000

    Fixed port display and track analysis

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    PORT = 8888;
 
 app.use(express.logger());
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-app.listen(8888);
-console.log('listening on port 8000');
+app.listen(PORT);
+console.log('listening on port ' + PORT);
