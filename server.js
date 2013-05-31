08eb279fc1d9e0ae0c8f698c0cca12d46bbdf539
EC2 Default User <ec2-user@ip-10-170-94-129.us-west-1.compute.internal>
2013-05-31 00:01:04 +0000

    Added audio playback to viz

--- server.js
+++ server.js
@@ -9,5 +9,10 @@ app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
+
+app.post('/api/v1/upload', function(req, res){
+  console.log(req.files);
+  res.send(200, 'track saved');
+});
 app.listen(PORT);
 console.log('listening on port ' + PORT);
